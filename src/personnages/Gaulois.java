048f858898c4a2083321732dd34fd7fe8de26cfb
AlexisDeCachard <alexisdecachard66740@gmail.com>
2023-09-21 15:14:36 +0200

    Romain + Druide + Chef + Début scénar

--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -4,5 +4,45 @@ public class Gaulois {
 	private String nom;
 	private int force;
 	private int effetPotion = 1;
+	
+	
+	public Gaulois(String nom, int force) {
+		this.nom = nom;
+		this.force = force;		
+	}
 
+		
+	public String getNom() {
+		return nom;
+	}
+
+	@Override
+	public String toString() {
+		return "Gaulois [nom=" + nom + ", force=" + force + ", effetPotion=" + effetPotion + "]";
+	}
+	
+	public void parler(String texte) {
+		System.out.println(prendreParole() + "« " + texte + " ».");
+	}
+	
+	public String prendreParole() {
+		return "Le gaulois " + nom + " : ";
+	}
+	
+	public void frapper(Romain romain) {
+		System.out.println(nom + " envoie un grand coup dans la mâchoire de " + romain.getNom());
+		romain.recevoirCoup(force/3);
+	}
+	public static void main(String[] args) {
+		Gaulois asterix = new Gaulois("Astérix", 8);
+		System.out.println(asterix.getNom());
+		System.out.println(asterix.toString());
+		System.out.println(asterix.prendreParole());
+		asterix.parler("Bonjour !");
+		Romain minus=new Romain("Minus",3);
+		asterix.frapper(minus);
+		asterix.frapper(minus);
+	}
+
+	
 }
