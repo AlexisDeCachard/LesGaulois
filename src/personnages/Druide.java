7b9ca2f066d59d2978ce69e32391f1cba9b38e53
AlexisDeCachard <alexisdecachard66740@gmail.com>
2023-09-21 15:31:57 +0200

    début prépa potion

--- src/personnages/Druide.java
+++ src/personnages/Druide.java
@@ -1,9 +1,13 @@
 package personnages;
 
+import java.util.Random;
+
 public class Druide {
 	private String nom;
 	private int effetPotionMin;
 	private int effetPotionMax;
+	private int forcePotion=1;
+	
 	public Druide(String nom, int effetPotionMin, int effetPotionMax) {
 		this.nom = nom;
 		this.effetPotionMin = effetPotionMin;
@@ -20,5 +24,15 @@ public class Druide {
 	
 	public String prendreParole() {
 		return "Le druide " + nom + " : ";
+	}
+	
+	public void preparerPotion() {
+		Random potion = new Random();
+		forcePotion = potion.nextInt(effetPotionMax);
+		if (forcePotion>7) {
+			parler("J'ai préparé une super potion de force " + forcePotion + ".");
+		} else {
+			parler("Je n'ai pas trouvé tous les ingrédients, ma potion est seulement de force " + forcePotion + ".");
+		}
 	}
 }
